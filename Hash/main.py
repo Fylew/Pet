d351afa10d878663efaf38f478f193905c547ed6
skile <skilet.e@yandex.ru>
2023-09-12 18:53:17 +0400

    in progres

--- Hash/main.py
+++ Hash/main.py
@@ -1,9 +1,10 @@
-import os
-
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication,QWidget
 import sys
 import hashlib
+
+
+text = ["`1234567890-=+_)(*&^%$#@!qwertyuiop[]asdfghjkl;'zxcvbnm,./QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?"]
 class App(QWidget):
     def __init__(self):
         self.start()
@@ -40,25 +41,32 @@ class App(QWidget):
         hash = self.main_menu.textEdit.toPlainText()  # мне нужно получить исходный хеш и потом кодировать слова под этот хеш
                                                     # и когда я получу соответсвите по хешам это и будет наш пароль
 
-        key = ""
-        if self.main_menu.comboBox_2.currentText() == "sha256":
-            key = hashlib.sha256(key.encode('utf-8')).hexdigest()
-        elif self.main_menu.comboBox_2.currentText() == "sha512":
-            key = hashlib.sha512(key.encode('utf-8')).hexdigest()
-        elif self.main_menu.comboBox_2.currentText() == "sha384":
-            key = hashlib.sha384(key.encode('utf-8')).hexdigest()
-        elif self.main_menu.comboBox_2.currentText() == "sha1":
-            key = hashlib.sha1(key.encode('utf-8')).hexdigest()
-        elif self.main_menu.comboBox_2.currentText() == "sha224":
-            key = hashlib.sha224(key.encode('utf-8')).hexdigest()
-        elif self.main_menu.comboBox_2.currentText() == "sha3_384":
-            key = hashlib.sha3_384(key.encode('utf-8')).hexdigest()
-        elif self.main_menu.comboBox_2.currentText() == "sha3_224":
-            key = hashlib.sha3_224(key.encode('utf-8')).hexdigest()
-        elif self.main_menu.comboBox_2.currentText() == "md5":
-            key = hashlib.md5(key.encode('utf-8')).hexdigest()
+        while True:
+            key = ''
+            for i in text:
+                if self.main_menu.comboBox_2.currentText() == "sha256":
+                    k = hashlib.sha256(key.encode('utf-8')).hexdigest()
+                elif self.main_menu.comboBox_2.currentText() == "sha512":
+                    k = hashlib.sha512(key.encode('utf-8')).hexdigest()
+                elif self.main_menu.comboBox_2.currentText() == "sha384":
+                    k = hashlib.sha384(key.encode('utf-8')).hexdigest()
+                elif self.main_menu.comboBox_2.currentText() == "sha1":
+                    k = hashlib.sha1(key.encode('utf-8')).hexdigest()
+                elif self.main_menu.comboBox_2.currentText() == "sha224":
+                    k = hashlib.sha224(key.encode('utf-8')).hexdigest()
+                elif self.main_menu.comboBox_2.currentText() == "sha3_384":
+                    k = hashlib.sha3_384(key.encode('utf-8')).hexdigest()
+                elif self.main_menu.comboBox_2.currentText() == "sha3_224":
+                    k = hashlib.sha3_224(key.encode('utf-8')).hexdigest()
+                elif self.main_menu.comboBox_2.currentText() == "md5":
+                    k = hashlib.md5(key.encode('utf-8')).hexdigest()
 
-        self.main_menu.textBrowser_3.setText(f"Скоро тут будет ваш пароль)")
+            if k == hash:
+                self.main_menu.textBrowser_3.setText(f"{key}")
+                break
+            else:
+                self.main_menu.textBrowser_3.setText(f"Скоро тут будет ваш пароль)")
+
 
     def Button(self):
         self.main_menu.pushButton.clicked.connect(lambda: self.encode())
